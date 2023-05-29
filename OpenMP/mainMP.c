5743945d4c3e59aeb6c52aad416d961da12800bd
Generalsava <zan24pockar@gmail.com>
2023-05-29 19:14:12 +0200

    Update mainMP.c
    this it it

--- OpenMP/mainMP.c
+++ OpenMP/mainMP.c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#include "constants.h"
+#include "../constants.h"
 #include "modelMP.h"
 #include <omp.h>
 
+// gcc -fopenmp -o mainMP mainMP.c --openmp -lm
 // 1. Začni z eno frozen celico, okoli nje so boundary
 // 2. Za vse celice, ki so boundary in unreceptive poteka difuzija
 // 3. Za vse celice, ki so frozen in boundary poteka konvekcija
@@ -14,17 +15,14 @@
 // 4. Preveri, če ima celica state >= 1 -> nastavi na frozen, njene sosede na boundary
 // 5. Preveri, če je boundary celica soseda z edge celico, prekini simulacijo
 
-void serial(Cell *cells)
+void paralel(Cell *cells)
 {
-#pragma omp barrier
-#pragma omp parallel for collapse(4) { // uporabil sem gnezdenje da se vsi štirje loop izvajajo, samo nisem prepričan če nebi bilo bolje brez ali pa samo 2
-                                       //  barrier condition sem dodal zato da se niti počakajo preden gremo naprej
-                                       // znotraj parralizacije sem vključil tudi edge pogoj preverjanje.
     float average = 0;
     double *stateTemp = (double *)malloc(NUM_CELLS * sizeof(double));
 
     for (int i = 0; i < STEPS; i++) // iteracije, oz stanja po casu
     {
+       #pragma omp parallel for reduction(+:average)
         for (int j = 0; j < NUM_CELLS; j++) // posodobi vsa stanja - difuzija, konvekcija
         {
             // We deal with one cell at the time, do not deal with edge type
@@ -38,6 +36,7 @@ void serial(Cell *cells)
             }
         }
 
+      #pragma omp parallel for
         for (int j = 0; j < NUM_CELLS; j++) // sedaj posodobi tipe celic
         {
             cells[j].state = stateTemp[j];
@@ -47,41 +46,19 @@ void serial(Cell *cells)
                 set_type_boundary(cells, cells[j].neighbors);
             }
         }
-
-        // Če je ena od sosed celice tipa edge, prekini simulacijo
-        for (int j = 0; j < NUM_CELLS; j++)
-        {
-            for (int k = 0; k < NUM_NEIGHBORS; k++)
-            {
-                if (cells[j].type == 1 && cells[j].neighbors[k] == 3)
-                {
-                    printf("break %d\n", i);
-                    i = STEPS;
-                    j = NUM_CELLS;
-                    break;
-                }
-            }
-        }
-
-        // printf("Step: %d ----------------------------------------------------------\n", i);
-
-        // for (int k = 0; k < NUM_CELLS; k++)
-        // {
-        //     if (cells[k].type == 0 || cells[k].type == 1)
-        //         printf("id: %d,\ttype: %d,\tstate: %lf\n", k, cells[k].type, cells[k].state);
-        // }
-        // printf("\n");
-
-        // draw_board(cells);
     }
     free(stateTemp);
-}
 }
 
 int main(int argc, int *argv[])
 {
     // // ------------- Začetek inicializacije ------------- //
     // printHexagon(ROWS); //
+      //#pragma omp parallel
+       //{
+   // #pragma omp single
+            //printf("num_threads = %d\n", omp_get_num_threads());
+       // }
 
     // Definicija arraya s structi
     Cell *cells = malloc(NUM_CELLS * sizeof *cells);
@@ -97,18 +74,15 @@ int main(int argc, int *argv[])
     clock_t start_time, end_time;
     start_time = clock();
 
-    draw_board(cells);
-    serial(cells);
-    draw_board(cells);
+   // draw_board(cells);
+    paralel(cells);
 
+    draw_board(cells);
     end_time = clock();
+
+    //draw_board(cells);
     printf("Time elapsed: %.3lf seconds\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
 
-    // Free allocated memory
-    for (int i = 0; i < NUM_CELLS; i++)
-    {
-        free(cells[i].neighbors);
-    }
     free(cells);
 
     return 0;
