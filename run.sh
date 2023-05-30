a31bc751f0cb45ac4e85e8376fb89dcb80b0606c
tomssojer <tom.sojer2@gmail.com>
2023-05-30 22:07:18 +0200

    Changes to bash file

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 if [ $# -lt 1 ]
 then
     echo 1>&2 "$0: file name not provided"
@@ -10,24 +8,14 @@ fi
 
 FILE=$1
 
-# Load modules
-module load CUDA/10.1.243-GCC-8.3.0
-module load OpenMPI/4.1.0-GCC-10.2.0
-
-# Compile programs
 gcc serijski/main.c -o run_serial
-gcc -fopenmp -o run_mp OpenMP/mainMP.c -lm
-nvcc cuda/mainCUDA.cu -O2 -o run_cuda
-mpicc openMPI/mainMPI.c -o run_row_mpi
-mpicc openMPI/columnMPI.c -o run_col_mpi
-
 echo -e "Starting simulation...\n\n" > $FILE
 
 # Serial algorithm
-echo -e "Running serial algorithm\n" >> $FILE
 srun --reservation=fri ./run_serial >> $FILE
 
 # openMP
+gcc -fopenmp -o run_mp OpenMP/mainMP.c -lm
 echo -e "\n-----------------------------\nRunning openMP\n" >> $FILE
 
 for i in 1 2 4 8 16 32
@@ -37,6 +25,8 @@ do
 done
 
 # cuda
+module load CUDA/10.1.243-GCC-8.3.0
+nvcc cuda/mainCUDA.cu -O2 -o run_cuda
 echo -e "\n-----------------------------\nRunning CUDA\n" >> $FILE
 
 for i in 16 32 64 128 256 512 1024
@@ -46,6 +36,8 @@ do
 done
 
 # openMPI by rows
+module load OpenMPI/4.1.0-GCC-10.2.0
+mpicc openMPI/mainMPI.c -o run_row_mpi
 echo -e "\n-----------------------------\nRunning MPI by rows\n" >> $FILE 
 
 for i in 1 2; do
@@ -58,15 +50,16 @@ for i in 1 2; do
     done
 done
 
-# openMPI by colums
-echo -e "\n-----------------------------\nRunning MPI by columns\n" >> $FILE
+# # openMPI by colums
+# echo -e "\n-----------------------------\nRunning MPI by columns\n" >> $FILE
+# mpicc openMPI/columnMPI.c -o run_col_mpi
 
-for i in 1 2; do
-    for j in 1 4 16 32; do
-        if [ $i != 2 ] || [ $j != 1 ]
-        then
-            echo "openMPI by columns - number of nodes = $i; number of processes = $j" >> $FILE
-            srun --reservation=fri --constraint=AMD  --mpi=pmix -n$j -N$i ./run_col_mpi >> $FILE
-        fi
-    done
-done
+# for i in 1 2; do
+#     for j in 1 4 16 32; do
+#         if [ $i != 2 ] || [ $j != 1 ]
+#         then
+#             echo "openMPI by columns - number of nodes = $i; number of processes = $j" >> $FILE
+#             srun --reservation=fri --constraint=AMD  --mpi=pmix -n$j -N$i ./run_col_mpi >> $FILE
+#         fi
+#     done
+# done
